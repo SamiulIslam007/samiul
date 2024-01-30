b39f7e50be0d67159d999ea8e33ab1b753e0365e
Samiul Islam <samiul10203040@gmail.com>
2024-01-30 20:18:03 +0600

    image added of me

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./Navbar";
+import samiulImg from "./samiul.jpeg";
 import { Typewriter } from "react-simple-typewriter";
 function Header() {
   return (
@@ -6,11 +7,18 @@ function Header() {
       <Navbar />
       <div className=" bg-bgPrimary h-[90vh] flex items-center justify-start">
         <div className="container mx-auto  text-center ">
-          <h1 className="text-[#ffffff] text-4xl font-semibold">
+          <div className="w-[200px] h-[200px] mx-auto ">
+            <img
+              src={samiulImg}
+              alt="samiul"
+              className=" rounded-[50%] bg-center border-[#0e1525] border-[7px] outline-8 outline outline-red-500"
+            />
+          </div>
+          <h1 className="text-[#ffffff] mt-7 text-3xl md:text-4xl font-semibold">
             Hi, I am{" "}
             <span className="text-[#d73131]">
               <Typewriter
-                words={["Samiul Islam."]}
+                words={["Samiul Islam.", "Joy."]}
                 loop={false}
                 cursor
                 cursorStyle="_"
@@ -20,11 +28,15 @@ function Header() {
               />
             </span>
           </h1>
-          <p className="text-[#d73131] mt-5 text-4xl font-semibold">
+          <p className="text-[#d73131] mt-5 text-2xl md:text-4xl font-semibold">
             I am a{" "}
             <span className="text-white">
               <Typewriter
-                words={["front end developer.", "web developer."]}
+                words={[
+                  "front end developer.",
+                  "web developer.",
+                  "programmer.",
+                ]}
                 loop={false}
                 cursor
                 typeSpeed={80}
