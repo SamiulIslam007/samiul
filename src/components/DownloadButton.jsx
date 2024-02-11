01d6a6e048dc55fa5874f66ec2a9da8df7b16a8f
Samiul Islam <samiul10203040@gmail.com>
2024-02-11 19:19:02 +0600

    img testing

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const DownloadButton = () => {
   const handleDownload = () => {
-    const apkUrl = "../../assets/samiul.apk";
+    const apkUrl = "../../images/samiul.jpeg";
 
     const tempAnchor = document.createElement("a");
     tempAnchor.href = apkUrl;
 
-    tempAnchor.setAttribute("download", "samiul.apk");
+    tempAnchor.setAttribute("download", "samiul.jpeg");
     tempAnchor.click();
   };
 
