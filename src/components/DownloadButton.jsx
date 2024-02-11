e6d83aaae27141e6a0287f3b65025b2c254c4537
Samiul Islam <samiul10203040@gmail.com>
2024-02-11 19:31:41 +0600

    the feature is not working so i have removed that

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const DownloadButton = () => {
   const handleDownload = () => {
-    const apkUrl = "../../images/samiul.jpeg";
+    const apkUrl = " ";
 
     const tempAnchor = document.createElement("a");
     tempAnchor.href = apkUrl;
 
-    tempAnchor.setAttribute("download", "samiul.jpeg");
+    tempAnchor.setAttribute("download");
     tempAnchor.click();
   };
 
